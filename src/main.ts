bb571a015bde028f954a45aa39571bc27351e042
choigeon <qrwrqrwrer@naver.com>
2025-01-17 10:42:28 +0900

    fix: cors 에러 설정

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,12 @@ const serverConfig = config.get('server');
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new CustomValidationPipe());
+  // CORS 활성화
+  app.enableCors({
+    origin: '*', // 허용할 도메인
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true, 
+  });
   setupSwagger(app);
   await app.listen(serverConfig.port ?? 3000);
 }
