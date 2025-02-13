588e6dfa20a4a28a1ba526389bcfe4f22c4f0aff
choigeon <qrwrqrwrer@naver.com>
2025-02-13 20:17:51 +0900

    fix: HTTPS 설정

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,37 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as config from 'config';
+import * as fs from 'fs';
 import { CustomValidationPipe } from './common/pipe/validationPipe.pipe';
 import { setupSwagger } from './swagger/swagger';
 
 const serverConfig = config.get('server');
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  // SSL 인증서 로드 (절대 경로로 설정)
+  const httpsOptions = {
+    key: fs.readFileSync(`${process.cwd()}/certs/private.key`),  // 개인 키
+    cert: fs.readFileSync(`${process.cwd()}/certs/certificate.crt`), // 인증서
+  };
+
+  // NestJS에서 HTTPS 적용
+  const app = await NestFactory.create(AppModule, { httpsOptions });
+
   app.useGlobalPipes(new CustomValidationPipe());
-  // CORS 활성화
+
   app.enableCors({
     origin: [
-        'https://soso-client-soso-web.vercel.app',
-        'http://127.0.0.1:5500',
+      'https://soso-client-soso-web.vercel.app',
+      'http://127.0.0.1:5500',
     ],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true, 
-});
+    credentials: true,
+  });
+
   setupSwagger(app);
-  await app.listen(serverConfig.port ?? 3000);
+
+  // HTTPS 포트 443 사용
+  await app.listen(serverConfig.port ?? 443);
 }
+
 bootstrap();
