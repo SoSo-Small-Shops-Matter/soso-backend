f27588e660e6a742a1adec0e75225a2a33f18b59
choigeon <qrwrqrwrer@naver.com>
2025-04-20 22:12:25 +0900

    fix: transaction 추가 - Submit 적용 완료 , Enum 추가 - SubmitStatus 및 SubmitType, UsingType

--- src/api/transactions/transactions.module.ts
+++ src/api/transactions/transactions.module.ts
@@ -1,15 +1,9 @@
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { Image } from '../../database/entity/image.entity';
-import { User } from '../../database/entity/user.entity';
-import { Wishlist } from '../../database/entity/wishlist.entity';
-import { SubmitUserRecord } from '../../database/entity/submit-user.entity';
-import { RecentSearch } from '../../database/entity/recent-search.entity';
 import { DeleteUserTransactionsRepository } from './delete-user.repository';
+import { SubmitTransactionsRepository } from './submit.repository';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Image, User, Wishlist, SubmitUserRecord, RecentSearch])],
-  providers: [DeleteUserTransactionsRepository],
-  exports: [DeleteUserTransactionsRepository],
+  providers: [DeleteUserTransactionsRepository, SubmitTransactionsRepository],
+  exports: [DeleteUserTransactionsRepository, SubmitTransactionsRepository],
 })
 export class TransactionsModule {}
--- src/common/enum/role.enum.ts
+++ src/common/enum/role.enum.ts
@@ -2,3 +2,20 @@ export enum Role {
   Admin = 'admin',
   User = 'user',
 }
+
+export enum SubmitType {
+  NewShop = 0,
+  NewOperating = 1,
+  NewProduct = 2,
+}
+
+export enum SubmitStatus {
+  Pending = 0,
+  Approved = 1,
+  Rejected = 2,
+}
+
+export enum UsingType {
+  Using = 0,
+  Verifying = 1,
+}
