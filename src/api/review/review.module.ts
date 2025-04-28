690a223fb3c715b9e48cb963b4bcfc3108899e4c
choigeon <qrwrqrwrer@naver.com>
2025-04-28 12:47:44 +0900

    fix: TransactionsModule Review Module에 추가 완료

--- src/api/review/review.module.ts
+++ src/api/review/review.module.ts
@@ -1,4 +1,4 @@
-import { Module, forwardRef } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { ReviewController } from './review.controller';
 import { ReviewService } from './review.service';
 import { AuthModule } from '../auth/auth.module';
@@ -8,9 +8,10 @@ import { ReviewRepository } from './review.repository';
 import { AwsModule } from '../aws/aws.module';
 import { Image } from 'src/database/entity/image.entity';
 import { ImageModule } from '../image/image.module';
+import { TransactionsModule } from '../transactions/transactions.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Review, Image]), AwsModule, ImageModule],
+  imports: [TypeOrmModule.forFeature([Review, Image]), AwsModule, ImageModule, TransactionsModule],
   controllers: [ReviewController],
   providers: [ReviewService, ReviewRepository],
   exports: [ReviewService, ReviewRepository],
