2ca99d474028511c2cd33ac7485ffda3e64975be
choigeon <qrwrqrwrer@naver.com>
2025-04-28 12:48:34 +0900

    fix: TransactionsModule Review Module에 추가 완료

--- src/api/review/review.module.ts
+++ src/api/review/review.module.ts
@@ -8,9 +8,10 @@ import { ReviewRepository } from './review.repository';
 import { AwsModule } from '../aws/aws.module';
 import { Image } from 'src/database/entity/image.entity';
 import { ImageModule } from '../image/image.module';
+import { TransactionsModule } from '../transactions/transactions.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Review, Image]), AwsModule, ImageModule],
+  imports: [TypeOrmModule.forFeature([Review, Image]), AwsModule, ImageModule, TransactionsModule],
   controllers: [ReviewController],
   providers: [ReviewService, ReviewRepository],
   exports: [ReviewService, ReviewRepository],
