44d6af117059971623cbfd3d27d992d10b49e938
choigeon <qrwrqrwrer@naver.com>
2025-07-23 12:40:53 +0900

    fix: User profile 불러오는 로직에 isNew 속성 추가

--- src/api/user/dto/user-response.dto.ts
+++ src/api/user/dto/user-response.dto.ts
@@ -3,11 +3,13 @@ export class ResponseUserProfileDTO {
   email: string;
   photoUrl: string;
   nickName: string;
+  isNew: boolean;
 
-  constructor({ uuid, email, photoUrl, nickName }) {
+  constructor({ uuid, email, photoUrl, nickName, isNew }) {
     this.uuid = uuid;
     this.email = email;
     this.photoUrl = photoUrl;
     this.nickName = nickName;
+    this.isNew = isNew;
   }
 }
